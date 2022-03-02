f052e11ec811a44fea89286427da276c02f9ffea
Jirayakbc <feliperobertorocha@gmail.com>
2022-03-02 11:45:01 -0300

    primeiro commit

---
+++ pokedex/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recomended',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'arrow-parents': ['error', 'as-needed', { requireForBlockBody : true}],
+    'import/no-extraneous-dependencies': 'off',
+    'ident': ['error' ,'tab'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-tabs': 'off',
+    'prefer-destructuring': ['error', {
+      'object': true,
+      'array': false,
+    }],
+    'vue/html-ident': ['error', 'tab'],
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }],
+    'vue/script-ident': ['error', 'tab',{
+      'baseIdent': 1,
+      'switchCase': 1,
+    }],
+    'vue/singleline-html-element-content-newline': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        ident: 'off',
+      },
+    },
+  ],
+};
---
+++ pokedex/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="pokedex">
+      <h1>Pokedex Felipe</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+#pokedex {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 24px;
+}
+</style>
